2935f509ca85e7ca101a66ca58a2ceb9ba0c8371
Paulo Martins <paulovmr@gmail.com>
2019-03-26 15:41:34 -0400

    save content

--- vscode/helloworld/src/extension.ts
+++ vscode/helloworld/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -10,6 +11,7 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	try {
 		init(context);
+
 		var eder = vscode.commands.registerCommand('workbench.action.files.save', function () {
 			console.info(`Saving KIE EDITOR` + new Date());
 			if (!openURI || openURI.length <= 0) {
@@ -17,7 +19,8 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			else {
 				console.log(`current url ` + openURI);
-				//paulo save aqui,
+				console.log(`>>> ${activePanel}`);
+				activePanel!.webview.postMessage({ type: "GET_CONTENT" });
 			}
 			let editor = vscode.window.activeTextEditor;
 			if (!editor) {
@@ -30,24 +33,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 		});
 		context.subscriptions.push(eder);
-
-		// vscode.commands.registerCommand('workbench.action.files.save', () => {
-		// console.info(`Saving KIE EDITOR`);
-		// return Promise.resolve();
-		// if(!openURI||openURI.length <= 0){
-		// 	console.log(`no url`);
-		// }
-		// else{
-		// 	console.log(`current url `+ openURI);
-		// }
-		// let editor = vscode.window.activeTextEditor;
-		// if (!editor) {
-		// 	return;
-		// }
-		// else{
-		// 	editor.document.save();
-		// }
-		// });
 	} catch (e) {
 		console.info(e);
 	}
@@ -55,6 +40,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 let saveHandler: vscode.Disposable;
 let openURI: string;
+let activePanel: vscode.WebviewPanel | undefined;
 
 function init(context: vscode.ExtensionContext) {
 
@@ -102,8 +88,43 @@ function init(context: vscode.ExtensionContext) {
 			'kie-submarine', // Identifies the type of the webview. Used internally
 			"KIE:: " + split[split.length - 1], // Title of the panel displayed to the user
 			{ viewColumn: vscode.ViewColumn.One, preserveFocus: true }, // Editor column to show the new webview panel in.
-			{ enableCommandUris: true } // Webview options. More on these later.
+			{ enableCommandUris: true, enableScripts: true, retainContextWhenHidden: true } // Webview options. More on these later.
 		);
+
+		activePanel = panel;
+
+		panel.webview.onDidReceiveMessage(
+			message => {
+				console.info('message received from webview');
+			    switch (message.type) {
+					case 'CONTENT':
+						console.info(`Effectivelly saved WebView on: ${uri.path}`); //Do saving operation for open WebViews
+				    	fs.writeFileSync(uri.path, message.content);
+				     	console.log("Content saved: " + message.content);
+				    	return;
+		        }
+			},
+			undefined,
+			context.subscriptions
+		);
+
+		// vscode.commands.registerCommand('workbench.action.files.save', () => {
+		// console.info(`Saving KIE EDITOR`);
+		// return Promise.resolve();
+		// if(!openURI||openURI.length <= 0){
+		// 	console.log(`no url`);
+		// }
+		// else{
+		// 	console.log(`current url `+ openURI);
+		// }
+		// let editor = vscode.window.activeTextEditor;
+		// if (!editor) {
+		// 	return;
+		// }
+		// else{
+		// 	editor.document.save();
+		// }
+		// });
 
 		console.info(`OPEN: registerig save handler for ${uri}`);
 
@@ -121,10 +142,12 @@ function init(context: vscode.ExtensionContext) {
 			if (panel.active) {
 				console.log("activating " + panel.title);
 				openURI = uri.fsPath;
+				activePanel = panel;
 				console.log(openURI);
 			}
 			else if (openURI === uri.fsPath) {
 				openURI = "";
+				activePanel = undefined;
 			}
 		});
 
@@ -132,14 +155,33 @@ function init(context: vscode.ExtensionContext) {
 			let source = document.getText();
 			panel.webview.html = `<!DOCTYPE html>
 			<html lang="en">
-			<head>
-					<meta charset="UTF-8">
-					<meta name="viewport" content="width=device-width, initial-scale=1.0">
-					<title>Cat Coding</title>
-			</head>
-			<body>
-					<textarea id="code">${source}</textarea>
-			</body>
+				<head>
+						<meta charset="UTF-8">
+						<meta name="viewport" content="width=device-width, initial-scale=1.0">
+						<title>KIE Editor</title>
+				</head>
+				<body>
+					<textarea id="source">${source}</textarea>
+					<script>
+
+						var sourceTextArea = document.getElementById("source");
+						const vscode = acquireVsCodeApi();
+						window.addEventListener('message', event => {
+							console.log(event);
+							const message = event.data; // The JSON data our extension sent
+
+							switch (message.type) {
+								case 'GET_CONTENT':
+									var content = sourceTextArea.value; // Replace to get actual editor content
+									vscode.postMessage({
+										type: "CONTENT",
+										content: content
+									});
+									break;
+							}
+						});
+					</script>
+				</body>
 			</html>`;
 		});
 	});
