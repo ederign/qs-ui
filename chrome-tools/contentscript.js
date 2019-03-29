376ffa108fa46037f7a71943356b18aca9affe3d
ederign <ignatowicz@gmail.com>
2019-03-29 19:40:41 -0400

    small try

--- chrome-tools/contentscript.js
+++ chrome-tools/contentscript.js
@@ -1,15 +1,47 @@
-console.log("Content script being executed!!");
+console.log("Content script being executed eder!");
 //hide current editor
-document.querySelector('.js-code-editor').style.display='none';
+let github = document.querySelector('.js-code-editor');
+if (github) {
+    console.log("github");
+    document.querySelector('.js-code-editor').style.display = 'none';
 // var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
 // if (!location.ancestorOrigins.contains(extensionOrigin)) {
-  var iframe = document.createElement('iframe');
-    // Must be declared at web_accessible_resourcess in manifest.json
-  iframe.src = chrome.runtime.getURL('frame.html');
- 
-  iframe.style.cssText = 'width:100%;height:100%;';
+    let iframe = document.createElement('iframe');
+    iframe.id = "parentIframe";
+    iframe.src = chrome.runtime.getURL('frame.html');
 
-  var oldEditor = document.querySelector('.file');
-  oldEditor.parentElement.insertBefore(iframe, oldEditor);
+    iframe.style.cssText = 'width:100%;height:100%;';
+    let oldEditor = document.querySelector('.file');
+    oldEditor.parentElement.insertBefore(iframe, oldEditor);
 
-// }
+    // window.addEventListener('message', function(event) {
+    //     console.log('page javascript got message:', event);
+    // });
+    //
+    // setTimeout(function() {
+    //     console.log('page javascript sending message');
+    //     window.postMessage({ type: 'page_js_type',
+    //             text: "Hello from the page's javascript!"},
+    //         '*' /* targetOrigin: any */);
+    // }, 2000);
+
+}
+else{
+
+    // window.addEventListener('message', function(event) {
+    //     console.log('content_script.js got message:', event);
+    //     // check event.type and event.data
+    // });
+    //
+    // setTimeout(function () {
+    //     window.gwtEditorBeans;
+    //     console.log('cs sending message');
+    //     window.postMessage({ type: 'content_script_type',
+    //             text: 'Hello from content_script.js!'},
+    //         '*' /* targetOrigin: any */ );
+    // }, 1000);
+
+}
+
+
+
