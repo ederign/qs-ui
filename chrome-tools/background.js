711cd2ef45690d6127facbe0d9cff06d974cde82
ederign <ignatowicz@gmail.com>
2019-03-30 11:22:16 -0400

    chrome extension

--- chrome-tools/background.js
+++ chrome-tools/background.js
@@ -12,3 +12,29 @@
 //     }]);
 //   });
 // });
+chrome.runtime.onInstalled.addListener(function() {
+    console.log("background");
+
+    setTimeout(function () {
+        console.log('log background');
+
+    }, 1000);
+});
+
+chrome.webRequest.onHeadersReceived.addListener(info => {
+    const headers = info.responseHeaders; // original headers
+    console.log("headers hack");
+    for (let i=headers.length-1; i>=0; --i) {
+        console.log("headers executed");
+        let header = headers[i].name.toLowerCase();
+        if (header === "content-security-policy") { // csp header is found
+            // modify frame-src here
+            headers[i].value = headers[i].value.replace("frame-src", "frame-src render.githubusercontent.com");
+        }
+    }
+    // return modified headers
+    return {responseHeaders: headers};
+}, {
+    urls: [ "<all_urls>" ], // match all pages
+    types: [ "sub_frame" ] // for framing only
+}, ["blocking", "responseHeaders"]);
--- chrome-tools/contentscript.js
+++ chrome-tools/contentscript.js
@@ -4,15 +4,40 @@ let github = document.querySelector('.js-code-editor');
 if (github) {
     console.log("github");
     document.querySelector('.js-code-editor').style.display = 'none';
-// var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
-// if (!location.ancestorOrigins.contains(extensionOrigin)) {
+
+
+    let meta = document.getElementsByTagName('meta')[0];
+    console.log(meta);
+    let newMeta = document.createElement('meta');
+    newMeta.httpEquiv="Content-Security-Policy";
+   // newMeta.content="default-src 'self' data: gap: https://ssl.gstatic.com 'unsafe-eval'; style-src 'self' 'unsafe-inline'; media-src *;**script-src 'self' http://127.0.0.1:8080 'unsafe-inline' 'unsafe-eval';** ";
+    newMeta.content="frame-src http://127.0.0.1:8080";
+
+
+
+    meta.parentElement.appendChild(newMeta);
+
+
     let iframe = document.createElement('iframe');
     iframe.id = "parentIframe";
-    iframe.src = chrome.runtime.getURL('frame.html');
-
+    iframe.src = "http://127.0.0.1:8080";
     iframe.style.cssText = 'width:100%;height:100%;';
     let oldEditor = document.querySelector('.file');
     oldEditor.parentElement.insertBefore(iframe, oldEditor);
+
+
+
+
+
+// var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+// if (!location.ancestorOrigins.contains(extensionOrigin)) {
+//     let iframe = document.createElement('iframe');
+//     iframe.id = "parentIframe";
+//     iframe.src = chrome.runtime.getURL('frame.html');
+//
+//     iframe.style.cssText = 'width:100%;height:100%;';
+//     let oldEditor = document.querySelector('.file');
+//     oldEditor.parentElement.insertBefore(iframe, oldEditor);
 
     // window.addEventListener('message', function(event) {
     //     console.log('page javascript got message:', event);
@@ -27,7 +52,7 @@ if (github) {
 
 }
 else{
-
+    console.log("eder");
     // window.addEventListener('message', function(event) {
     //     console.log('content_script.js got message:', event);
     //     // check event.type and event.data
